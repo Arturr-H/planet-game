63e67e326d937178d1852aa2be1e9803ae16834e
Arturr-H <artur.h.0427@gmail.com>
2025-01-09 15:05:50 +0100

    Fix better `Damageable` animated labels
    Labels stack up from previous hits

--- src/functional/damageable/damageable.rs
+++ src/functional/damageable/damageable.rs
@@ -1,7 +1,7 @@
 use std::f32::consts::PI;
 
 /* Imports */
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashMap};
 use rand::Rng;
 use crate::{components::planet::{Planet, PlayerPlanet}, systems::game::PlanetResource, utils::{audio::{game_sounds, play_audio}, color::hex, logger}};
 
@@ -16,6 +16,16 @@ pub struct Damageable {
     drop: Option<(PlanetResource, usize)>
 }
 
+/// This stores all damages done for entites,
+/// because every time we do damage to an entity
+/// we don't want to spawn a new label, but rather
+/// update the existing one.
+#[derive(Resource)]
+pub struct DamageLabels {
+    /// HashMap<Target entity (e.g tree), (Label entity, accumulated damage)>
+    pub labels: HashMap<Entity, (Entity, f32)>,
+}
+
 /// Component for damage impact effect 
 #[derive(Component)]
 pub struct Flashing;
@@ -23,7 +33,7 @@ pub struct Flashing;
 /// Event for damaging entities
 #[derive(Event)]
 pub struct DamageEvent {
-    pub entity: Entity,
+    pub target_entity: Entity,
     pub damage: f32,
 }
 
@@ -41,15 +51,24 @@ impl Damageable {
         click: Trigger<Pointer<Down>>,
         mut commands: Commands,
         mut damage_events: ResMut<Events<DamageEvent>>,
-        mut query: Query<(Entity, &mut AnimatedDamageText, &mut Sprite, &mut Transform)>,
+        mut damage_labels: ResMut<DamageLabels>,
+        mut damage_text_q: Query<&mut AnimatedDamageText>,
         asset_server: Res<AssetServer>,
     ) {
-        let entity = click.entity();
+        let target_entity = click.entity();
         let damage = (rand::random::<f32>() * 5.0 + 3.0).floor();
-        damage_events.send(DamageEvent { entity, damage });
-        commands.entity(click.entity()).insert(Flashing);
-        AnimatedDamageText::spawn_damage_text(&mut commands, &asset_server, entity, damage);
+        damage_events.send(DamageEvent { target_entity, damage });
 
+        /* Visual */
+        commands.entity(click.entity()).insert(Flashing);
+        AnimatedDamageText::spawn_damage_text(
+            &mut commands,
+            &asset_server,
+            &mut damage_labels,
+            &mut damage_text_q,
+            target_entity,
+            damage
+        );
         play_audio(&mut commands, &asset_server, game_sounds::tree::DAMAGE, false);
     }
     
@@ -62,7 +81,7 @@ impl Damageable {
         // Collect entities to process
         let mut entities_to_process = Vec::new();
         for event in reader.read(&damage_events) {
-            entities_to_process.push((event.entity, event.damage));
+            entities_to_process.push((event.target_entity, event.damage));
         }
         damage_events.clear();
     
@@ -73,7 +92,6 @@ impl Damageable {
             let Some(mut damageable) = entity_mut.get_mut::<Damageable>() else { continue; };
 
             damageable.health -= damage;
-            println!("{} healt -{}", damageable.health, damage);
             if damageable.health <= 0.0 {
                 let drop = damageable.drop.clone();
                 callback = Some(damageable.callback);
@@ -111,6 +129,7 @@ impl Damageable {
 
 #[derive(Component)]
 pub struct AnimatedDamageText {
+    alpha_timer: Timer,
     timer: Timer,
     rotation: f32,
     speed: f32,
@@ -121,48 +140,107 @@ impl AnimatedDamageText {
     fn spawn_damage_text(
         commands: &mut Commands,
         asset_server: &Res<AssetServer>,
+        damage_labels: &mut ResMut<DamageLabels>,
+        damage_text_q: &mut Query<&mut AnimatedDamageText>,
         target_entity: Entity,
         damage: f32
     ) {
         let mut rng = rand::thread_rng();
-    
-        let texture = asset_server.load(format!("numbers/{damage}.png"));
         let animated_text = AnimatedDamageText {
             timer: Timer::from_seconds(1.0, TimerMode::Once),
+            alpha_timer: Timer::from_seconds(1.0, TimerMode::Once),
             rotation: PI / 2.0 + rng.gen_range(-0.4..0.4),
             speed: rng.gen_range(0.8..1.2),
             damage,
         };
-        commands.entity(target_entity).with_children(|parent| {
-            parent.spawn((
-                Sprite {
-                    image: texture,
-                    ..Default::default()
-                },
-                Transform::from_translation(Vec3::new(0.0, 32.0, 10.0)),
-                animated_text,
-            ));
+    
+        let (shell_entity, new_damage) = match damage_labels.labels.get_mut(&target_entity) {
+            Some((prev_label_entity, accumulated_damage)) => {
+                *accumulated_damage += damage;
+                commands.get_entity(*prev_label_entity).map(|mut e| { e.despawn_descendants(); });
+                (*prev_label_entity, *accumulated_damage)
+            },
+            None => {
+                (Entity::PLACEHOLDER, damage)
+            }
+        };
+
+        if let Ok(mut damage_text) = damage_text_q.get_mut(shell_entity) {
+            damage_text.alpha_timer.reset();
+        }
+
+        let mut shell_entity = match commands.get_entity(shell_entity) {
+            Some(e) => e,
+
+            /* Create a new shell entity */
+            None => {
+                let mut shell_entity = None;
+                commands.entity(target_entity).with_children(|parent| {
+                    shell_entity = Some(parent.spawn((
+                        animated_text,
+                        Transform::from_translation(Vec3::new(0.0, 32.0, 10.0)),
+                    )).id());
+                });
+
+                match shell_entity.and_then(|e| commands.get_entity(e)) {
+                    Some(e) => e,
+                    None => return,
+                }
+            }
+        };
+        
+        let chars = (new_damage as usize).to_string().chars().collect::<Vec<char>>();
+        chars.iter().enumerate().for_each(|(i, c)| {
+            const NUMBER_WIDTH: f32 = 16.0;
+            let start = chars.len() as f32 * NUMBER_WIDTH / 2.0;
+            let step = NUMBER_WIDTH * i as f32 * 0.75;
+
+            shell_entity.with_children(|parent| {
+                parent.spawn((
+                    Sprite {
+                        image: asset_server.load(format!("numbers/{}.png", c)),
+                        ..Default::default()
+                    },
+                    Transform::from_translation(Vec3::new(- start + step, 0.0, 0.0))
+                ));
+            });
         });
+
+        damage_labels.labels.insert(target_entity, (shell_entity.id(), new_damage));
     }
 
     fn update_text_animation(
         mut commands: Commands,
         time: Res<Time>,
-        mut query: Query<(Entity, &mut AnimatedDamageText, &mut Transform, &mut Sprite)>,
+        mut query: Query<(&Parent, Entity, &mut AnimatedDamageText, &mut Transform, &Children)>,
+        mut query_opacity: Query<&mut Sprite>,
+        mut damage_labels: ResMut<DamageLabels>,
     ) {
-        for (entity, mut animated_text, mut transform, mut sprite) in query.iter_mut() {
+        for (target_entity, shell_entity, mut animated_text, mut transform, children) in query.iter_mut() {
             animated_text.timer.tick(time.delta());
+            animated_text.alpha_timer.tick(time.delta());
     
             let progress = animated_text.timer.elapsed_secs() / animated_text.timer.duration().as_secs_f32();
+            let alpha_timer_progress = animated_text.alpha_timer.elapsed_secs() / animated_text.alpha_timer.duration().as_secs_f32();
     
             let ext = 50.0 * (1.0 - progress) * time.delta_secs() * animated_text.speed;
             transform.translation.y += ext * animated_text.rotation.sin();
             transform.translation.x += ext * animated_text.rotation.cos();
             transform.translation.z -= ext / 1000.0;
-            let alpha = ((2.0 - progress * 2.0).min(1.0)).powi(2);
-            sprite.color.set_alpha(alpha);
-            if animated_text.timer.finished() {
-                // commands.entity(entity).despawn();
+            let alpha = ((2.0 - alpha_timer_progress * 2.0).min(1.0)).powi(2);
+            for child in children.iter() {
+                if let Ok(mut sprite) = query_opacity.get_mut(*child) {
+                    sprite.color.set_alpha(alpha);
+                }
+            }
+            if animated_text.alpha_timer.finished() {
+                match commands.get_entity(shell_entity) {
+                    Some(e) => {
+                        e.despawn_recursive();
+                        damage_labels.labels.remove(&target_entity.get());
+                    },
+                    None => (),
+                }
             }
         }
     }
@@ -173,6 +251,7 @@ pub struct DamageablePlugin;
 impl Plugin for DamageablePlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(DamageLabels { labels: HashMap::default() })
             .add_event::<DamageEvent>()
             .add_systems(Update, (
                 Damageable::apply_damage,
